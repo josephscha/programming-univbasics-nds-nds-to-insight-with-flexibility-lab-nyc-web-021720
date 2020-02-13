2b77319e43d58f6cf2b66450529e5fd56d209e38
Joseph S. Cha <josephshincha@gmail.com>
2020-02-13 21:37:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -71,6 +71,12 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+  result = {}
+  index = 0 
+  while index < collection.length do 
+    movie = collection[index]
+    
+    
 end
 
 def movies_with_directors_set(source)
